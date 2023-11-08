ca348efcb858b4752346653e66fc85a6f915cbc1
Alejandro Romero <arome90@users.noreply.github.com>
2023-11-08 11:16:45 -0600

    Json returned

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import textComp
+import json
 
 app = Flask(__name__)
 @app.route('/api/textos/', methods=["GET", "POST"])
@@ -10,12 +11,10 @@ def getInformationAndProcess():
     if request.method == 'POST':
         contentJson = request.json
         titulo1 = str(contentJson['Mexico'][0])
-        response = ''
-        #for field_dict in contentJson['Mexico']:
-        #    str(textComp.calcular_similitud(field_dict, field_dict))
-        #return str(titulo1)
-        return 'Numero de elementos ' + str(len(contentJson['Mexico'])) + ' La similitud entre ' + str(contentJson['Mexico'][0]['titulo']) + ' y ' +str(contentJson['Mexico'][1]['titulo']) + ' es de ' + str(textComp.calcular_similitud(contentJson['Mexico'][0]['letra'], contentJson['Mexico'][1]['letra']))
-    
+        response = {}
+
+        response["TextSimilarity"] = textComp.calcular_similitud(contentJson['Mexico'][0]['letra'], contentJson['Mexico'][1]['letra'])
+        return response    
 
 @app.route('/')
 def getBasicInformation():
