2fbf5fbf1b5daa69839dbe49b3da759f10ff9fd2
Alejandro Romero <arome90@users.noreply.github.com>
2023-11-07 21:30:21 -0600

    Server y API

--- app.py
+++ app.py
@@ -4,10 +4,21 @@ import textComp
 app = Flask(__name__)
 @app.route('/api/textos/', methods=["GET", "POST"])
 def getInformationAndProcess():
-    contentJson = request.json
-    titulo1 = str(contentJson['Mexico'][0])
-    response = ''
-    #for field_dict in contentJson['Mexico']:
-    #    str(textComp.calcular_similitud(field_dict, field_dict))
-    #return str(titulo1)
-    return 'Numero de elementos ' + str(len(contentJson['Mexico'])) + ' La similitud entre ' + str(contentJson['Mexico'][0]['titulo']) + ' y ' +str(contentJson['Mexico'][1]['titulo']) + ' es de ' + str(textComp.calcular_similitud(contentJson['Mexico'][0]['letra'], contentJson['Mexico'][1]['letra']))
+
+    if request.method == 'GET':
+        return 'Esto es la api de Digifolk! Por favor, elige la opción y manda la información correcta'
+    if request.method == 'POST':
+        contentJson = request.json
+        titulo1 = str(contentJson['Mexico'][0])
+        response = ''
+        #for field_dict in contentJson['Mexico']:
+        #    str(textComp.calcular_similitud(field_dict, field_dict))
+        #return str(titulo1)
+        return 'Numero de elementos ' + str(len(contentJson['Mexico'])) + ' La similitud entre ' + str(contentJson['Mexico'][0]['titulo']) + ' y ' +str(contentJson['Mexico'][1]['titulo']) + ' es de ' + str(textComp.calcular_similitud(contentJson['Mexico'][0]['letra'], contentJson['Mexico'][1]['letra']))
+    
+
+@app.route('/')
+def getBasicInformation():
+
+    if request.method == 'GET':
+        return 'Esto es el servidor de Digifolk!'
