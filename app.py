75b11911fb06264d22b42dd8475de7acf3e53be6
Alejandro Romero <arome90@users.noreply.github.com>
2023-11-24 12:27:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -16,13 +16,13 @@ def getInformationAndProcess():
         titulo1 = str(contentJson[0])
         response = {}
 
-        response["TextSimilarity"] = textComp.calcular_similitud(contentJson['Mexico'][0]['letra'], contentJson['Mexico'][1]['letra'])
-        response["levenshteinCustom"] = LevenshteinDistances.levenshteinDistanceDP(contentJson['Mexico'][0]['letra'], contentJson['Mexico'][1]['letra'])
-        response["levenshteinHermetrics"] = hermetricsMed.hermetricsLevenstein(contentJson['Mexico'][0]['letra'], contentJson['Mexico'][1]['letra'])
-        response["comparativaHermetrics"] = hermetricsMed.hermetricsComp(contentJson['Mexico'][0]['letra'], contentJson['Mexico'][1]['letra'])
-        response["SemanticTextSimilarity"] = SemanticTextSimilarity.predictionComparision(contentJson['Mexico'][0]['letra'], contentJson['Mexico'][1]['letra'])
-        #response["SimilarityCheck"] = SimilarityCheckMed.similarityChecker(contentJson['Mexico'][0]['letra'], contentJson['Mexico'][1]['letra'])
-        response["JensenShanon2"] = TopicModelingGensim2.calcularGensim(contentJson['Mexico'], contentJson['Mexico'][0]['letra'])
+        response["TextSimilarity"] = textComp.calcular_similitud(contentJson[0]['letra'], contentJson[1]['letra'])
+        response["levenshteinCustom"] = LevenshteinDistances.levenshteinDistanceDP(contentJson[0]['letra'], contentJson[1]['letra'])
+        response["levenshteinHermetrics"] = hermetricsMed.hermetricsLevenstein(contentJson[0]['letra'], contentJson[1]['letra'])
+        response["comparativaHermetrics"] = hermetricsMed.hermetricsComp(contentJson[0]['letra'], contentJson[1]['letra'])
+        response["SemanticTextSimilarity"] = SemanticTextSimilarity.predictionComparision(contentJson[0]['letra'], contentJson[1]['letra'])
+        #response["SimilarityCheck"] = SimilarityCheckMed.similarityChecker(contentJson[0]['letra'], contentJson[1]['letra'])
+        response["JensenShanon2"] = TopicModelingGensim2.calcularGensim(contentJson, contentJson[0]['letra'])
         return response    
 
 @app.route('/')
