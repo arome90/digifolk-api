25a998c3b38a263cf9bf52ad0db36a902b1285e7
Alejandro Romero <arome90@users.noreply.github.com>
2023-11-24 12:23:16 -0600

    Update TopicModelingGensim2.py

--- TopicModelingGensim2.py
+++ TopicModelingGensim2.py
@@ -8,7 +8,6 @@ from gensim.models import LdaModel
 import random
 import numpy as np
 import matplotlib.pyplot as plt
-from wordcloud import WordCloud
 from gensim.matutils import jensen_shannon
 
 
